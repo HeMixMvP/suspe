c1d0610d3b2e7002e17efc634692eb4b8a2cd682
HeMixMvP <44098198+HeMixMvP@users.noreply.github.com>
2018-12-08 22:40:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "c";
-var adminprefix = 'c'
+var prefix = "t";
+var adminprefix = 't'
 
 const developers = ["527231808861896704","id"]
 client.on('message', message => {
